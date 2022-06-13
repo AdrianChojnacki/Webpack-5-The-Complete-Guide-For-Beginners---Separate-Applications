52bc9c9733cdc552ce3a2fa15a6a34e2e3ac10c9
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-13 11:24:55 +0200

    some error

--- hello-world/src/components/hello-world-page/hello-world-page.js
+++ hello-world/src/components/hello-world-page/hello-world-page.js
@@ -1,13 +1,13 @@
-import HelloWorldButton from "../hello-world-button/hello-world-button.js";
-import Heading from "../heading/heading.js";
+import HelloWorldButton from '../hello-world-button/hello-world-button.js';
+import Heading from '../heading/heading.js';
 
 class HelloWorldPage {
-  render() {
-    const heading = new Heading();
-    heading.render();
-    const helloWorldButton = new HelloWorldButton();
-    helloWorldButton.render();
-  }
+    render() {
+        const heading = new Heading();
+        heading.render('hello world');
+        const helloWorldButton = new HelloWorldButton();
+        helloWorldButton.render();
+    }
 }
 
 export default HelloWorldPage;
